e82947cd8c3c0b7ac5e63d743359a2a10858196a
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-10-23 15:31:41 +0300

    Update functions.py

---
+++ lab2/py_code/functions.py
@@ -0,0 +1,40 @@
+import numpy as np
+from math import *
+
+m_1 = np.array([[-0.6, 0.8], [0.8, 0.6]])
+
+m_2 = np.array([[1, 0], [8, 0]])
+
+m_3 = np.array([[cos(5*pi/18), -sin(5*pi/18)], [sin(5*pi/18), cos(5*pi/18)]])
+
+m_4 = np.array([[-1, 0], [0, -1]])
+
+m_5 = np.array([[-0.3*sqrt(3) + 0.4, 0.4*sqrt(3) + 0.3], [0.4*sqrt(3) + 0.3, 0.3*sqrt(3) - 0.4]])
+
+m_6 = np.array([[1, 1/8], [2, 1]])
+
+m_7 = np.array([[4/3, -1/6], [-8/3, 4/3]])
+
+m_8 = np.array([[1, 0], [10, -1]])
+
+m_9 = np.array([[sqrt(5), 0], [sqrt(5), 0]])
+
+m_10 = np.array([[3, 0], [0, 1]])
+
+m_11 = np.array([[6/5, 2/5], [2/5, 9/5]])
+
+m_12 = np.array([[4, 1], [0, 4]])
+
+m_13 = np.array([[0, -1], [4, 0]])
+
+m_14 = np.array([[0, 0], [0, 0]])
+
+m_15_a = np.array([[1, 2], [3, 4]])
+
+m_15_b = np.array([[5, 6], [7, 8]])
+
+m_16_a = np.array([[1, 4], [0, 2]])
+
+m_16_b = np.array([[9, 8], [0, 11]])
+
+
