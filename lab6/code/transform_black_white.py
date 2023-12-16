235d2abd9577ec46bb4ee8fbb3e5ab6d1b6378bc
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-12-17 01:08:21 +0300

    finish ^. .%

--- lab6/code/transform_black_white.py
+++ lab6/code/transform_black_white.py
@@ -26,10 +26,17 @@ matrix_image = mpimg.imread('bw_red.png')
 U, s, W = np.linalg.svd(matrix_image)
 s = np.diag(s)
 
-k = [20000, 4500, 1000, 250, 150, 100, 50, 20, 10, 5, 1]
+k = [20000, 4500, 1000, 250, 100, 50, 20, 10, 5, 1]
 
 for i in k:
-    cut = U[:, :i] @ s[0:i, :i] @ W[:i, :]
+    cut = np.dot(np.dot(U[:, :i], s[:i, :i]), W[:i, :])
+    orig_size = np.shape(U)[0] * np.shape(U)[1] + np.shape(s)[0] + np.shape(W)[0] * np.shape(W)[1]
+    # print(orig_size)
+    # print('\n')
+    cut_size = np.shape(U[:, :i])[0] * np.shape(U[:, :i])[1] + np.shape(s[0:i, :i])[0] + np.shape(W[:i, :])[0] * np.shape(W[:i, :])[1]
+    print(100 * (1 - cut_size / orig_size))
+    print('---------------')
+    print('\n')
 
     fig, axs = plt.subplots(1, 2)
     axs[0].axis('off')
@@ -43,4 +50,4 @@ for i in k:
     plt.show()
 
 
-#mpimg.imsave('after_svd.png', np.dot(np.dot(U, D), V.T))
+ #mpimg.imsave('after_svd.png', np.dot(np.dot(U, D), V.T))
