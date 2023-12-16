fc25d2e6be39ba0ddf8faf2b7f3e6d4d9106ecba
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-12-16 16:12:36 +0300

    in process...

--- lab6/code/transform_black_white.py
+++ lab6/code/transform_black_white.py
@@ -4,17 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def result_drawer(orig, comp, K):
+'''def result_drawer(orig, cut_m, k_param):
     fig, axs = plt.subplots(1, 2)
     axs[0].axis('off')
     axs[1].axis('off')
 
     axs[0].imshow(orig).set_cmap('grey')
-    axs[1].imshow(comp).set_cmap('grey')
+    axs[1].imshow(cut_m).set_cmap('grey')
 
     axs[0].set_title('Оригинал')
-    axs[1].set_title('После сжатия, k=' + str(K))
-    plt.show()
+    axs[1].set_title('После сжатия, k=' + str(k_param))
+    plt.show()'''
 
 
 img = Image.open('red_eye.png')
@@ -29,9 +29,18 @@ s = np.diag(s)
 k = [20000, 4500, 1000, 250, 150, 100, 50, 20, 10, 5, 1]
 
 for i in k:
-    compress = U[:, :i] @ s[0:i, :i] @ W[:i, :]
-    result_drawer(matrix_image, compress, i)
+    cut = U[:, :i] @ s[0:i, :i] @ W[:i, :]
 
+    fig, axs = plt.subplots(1, 2)
+    axs[0].axis('off')
+    axs[1].axis('off')
+
+    axs[0].imshow(matrix_image).set_cmap('grey')
+    axs[1].imshow(cut).set_cmap('grey')
+
+    axs[0].set_title('Оригинал')
+    axs[1].set_title('После сжатия, k=' + str(i))
+    plt.show()
 
 
 #mpimg.imsave('after_svd.png', np.dot(np.dot(U, D), V.T))
